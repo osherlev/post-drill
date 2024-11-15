634047b5efc7d26c54ea48c7eaf5b14861a4d253
osherlev <osherlev317@gmail.com>
2024-11-15 19:38:04 +0200

    fix: renaming model field

--- models/post.js
+++ models/post.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const postSchema = new mongoose.Schema({
     title: {type: String, required: true},
     content: String,
-    owner: {type: String, required: true},
+    sender: {type: String, required: true},
 });
 
 const postModel = mongoose.model("Posts", postSchema);
