4b9c3e174eb2758a8c562bc1a297b9cd7999122b
osherlev <osherlev317@gmail.com>
2024-11-15 18:46:17 +0200

    fix: removing unused dependency

--- models/post.js
+++ models/post.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-    title: {type: String, required: true,},
+    title: {type: String, required: true},
     content: String,
-    owner: {type: String, required: true,},
+    owner: {type: String, required: true},
 });
 
 const postModel = mongoose.model("Posts", postSchema);
