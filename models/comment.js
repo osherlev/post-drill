298d07ee2e2e6ff7ced8b7c036de058bc595a7b9
ShadowJu <alikkhol@gmail.com>
2024-11-17 21:42:39 +0200

    comments-api/change comment scheme and both mongo models name

--- models/comment.js
+++ models/comment.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema({
   sender: { type: String, required: true },
-  postId: { type: String, required: true },
+  postId: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
   content: String,
-  timestamp: { type: Date, default: Date.now },
+  date: { type: Date, default: Date.now },
 });
 
-const commentModel = mongoose.model("Comments", commentSchema);
+const commentModel = mongoose.model("Comment", commentSchema);
 
 module.exports = commentModel;
