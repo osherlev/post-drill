1d41b5003afb8a339ed17db7808e09d01c990d5f
osherlev <osherlev317@gmail.com>
2024-11-18 21:34:45 +0200

    fix: remove post from routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -6,10 +6,10 @@ router.post("/createPost", postController.createPost);
 
 router.get("/getAllPosts", postController.getAllPosts);
 
-router.get("/post/:id", postController.getPostById);
+router.get("/:id", postController.getPostById);
 
 router.get("/post", postController.getPostsBySender);
 
-router.put("/post/:id", postController.updatePost);
+router.put("/:id", postController.updatePost);
 
 module.exports = router;
