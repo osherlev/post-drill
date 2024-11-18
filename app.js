148aaa352fa983b95cc706c0c376ddbbf57d143e
osherlev <osherlev317@gmail.com>
2024-11-18 21:35:03 +0200

    feat: add env configuration

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 const port = process.env.PORT;
 
 const mongoose = require("mongoose");
